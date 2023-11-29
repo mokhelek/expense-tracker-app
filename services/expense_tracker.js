712418cac1974557a7cc645b3ef472adfb2b4268
mokhelek <mkatleho191@gmail.com>
2023-11-29 16:12:55 +0200

    add code documentation

--- services/expense_tracker.js
+++ services/expense_tracker.js
@@ -1,4 +1,9 @@
 export default function expenseTrackerService(db) {
+    /*
+        function to retrieve all the expenses
+        retrieve the category details and
+        expense details
+    */
     async function allExpenses() {
         const expenses = await db.any(`
             SELECT
@@ -15,11 +20,21 @@ export default function expenseTrackerService(db) {
             return expenses;
     }
 
+     /*
+        function to retrieve all the categories
+        Select all the data for each category
+    */
     async function allCategories() {
         const categories = await db.many("SELECT * FROM category");
         return categories;
     }
 
+     /*
+        function to get the total amounts for each category
+        Join the expense data to category
+        retrieve Category ID 
+        SUM the totals for each category
+    */
     async function categoryTotals() {
         const categoryTotals = await db.any(`
             SELECT
@@ -39,6 +54,11 @@ export default function expenseTrackerService(db) {
         return categoryTotals;
     }
 
+     /*
+        function for adding a new expense
+        add amount, category ID and description
+        Calculate the total for each expense
+    */
     async function addExpense(amount, categoryId, description) {
         const categoryQuantity = {
             1: 4,
@@ -51,10 +71,19 @@ export default function expenseTrackerService(db) {
         await db.none("INSERT INTO expense(amount, category_id, expense, total) VALUES($1, $2, $3, $4)", [amount, categoryId, description, amount * categoryQuantity[categoryId]]);
     }
 
+     /*
+        function to delete each expense
+        delete the expense based on ID number
+    */
     async function deleteExpense(expenseID) {
          await db.none("DELETE FROM expense WHERE id = $1",[expenseID]);
     }
 
+     /*
+        function to filter the expenses based on category
+        get the category details and expenses details
+        SUM the totals 
+    */
     async function expenseForCategory(categoryId) {
        
         const query = `
